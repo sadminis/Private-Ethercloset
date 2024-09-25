46eadc038f09f9595c9b5cd18b7a218094851887
Justin Chan <70335804+sadminis@users.noreply.github.com>
2024-09-25 01:28:04 -0600

    Add Classes

---
+++ Private-Ethercloset/Core/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Private_Ethercloset.Core
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
