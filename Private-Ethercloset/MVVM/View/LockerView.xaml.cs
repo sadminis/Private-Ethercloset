c4c3d685788df730fe3fd6e7f1aaeacacba56451
Justin Chan <70335804+sadminis@users.noreply.github.com>
2024-09-28 12:28:19 -0600

    view models created

--- Private-Ethercloset/MVVM/View/LockerView.xaml.cs
+++ Private-Ethercloset/MVVM/View/LockerView.xaml.cs
@@ -13,21 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Private_Ethercloset
+namespace Private_Ethercloset.MVVM.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LockerView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LockerView : UserControl
     {
-        public MainWindow()
+        public LockerView()
         {
             InitializeComponent();
-        }
-
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
---
+++ Private-Ethercloset/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Private_Ethercloset.Core;
+using Private_Ethercloset.MVVM.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Private_Ethercloset.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public HomeViewModel HomeVM { get; set; }
+        public LockerView LockerVM { get; set; }
+
+
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand LockerViewCommand { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel() { 
+            HomeVM = new HomeViewModel();
+
+            LockerVM = new LockerView();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            LockerViewCommand = new RelayCommand(o =>
+            { 
+                CurrentView = LockerVM;
+            });
+        }
+    }
+}
